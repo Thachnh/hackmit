f3c9fe5e0f3261ac3946408905b2daf5269cb8ff
Thach Nguyen <thachnhus@gmail.com>
2013-10-05 19:58:56 -0400

    Tour view page

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ TourStarter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'tours#index'
 
   # See how all your routes lay out with "rake routes"
 
