0d9435e4d6239b09a2708fca6506703fe85e9cab
quocle <quocle@no.reply.sorry.com>
2013-10-05 15:04:44 -0400

    First commit

---
+++ tour_starter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TourStarter::Application.config.secret_token = 'b11bc29ce9ef0b75a9b11307f6a1badd1a59839ef62796bc59c9094df9c9263ed85c759a7878ab92884fb5b0cd7bb06694f4e133bb8f8c3268dc9891d8684700'
