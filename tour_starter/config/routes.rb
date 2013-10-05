2e0d8f7b1cc3ee75d0620b57810402ec22c81770
Thach Nguyen <thachnhus@gmail.com>
2013-10-05 16:12:51 -0400

    Landing page

--- tour_starter/config/routes.rb
+++ tour_starter/config/routes.rb
@@ -1,4 +1,6 @@
 TourStarter::Application.routes.draw do
+  get "home/index"
+
   resources :tours
 
   devise_for :users
@@ -52,7 +54,7 @@ TourStarter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
