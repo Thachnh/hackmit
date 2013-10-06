a9e1d4789d013e479ac702454373458dd3d8115a
Duc Nguyen <duc@ninja.(none)>
2013-10-05 23:40:09 -0400

    bootstrap-typeahead added for input

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require twitter/bootstrap
 //= require_tree .
 //= require flat-ui
+
+$(document).ready(function() {
+  $('#inputLocation').typeahead( {
+  	items : 10,
+  	source : [ 'Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Dakota','North Carolina','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+  });
+
+  $('#inputTour').typeahead( {
+  	items : 10,
+  	source : [ 'Book', 'Historic', 'Food', 'Nature', 'Sports' ]
+  });
+
+});
