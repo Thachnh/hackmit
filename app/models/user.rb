74a0e347ccd724cfab70acc57f280c64a41b090e
quocle <quocle@no.reply.sorry.com>
2013-10-06 01:51:50 -0400

    Fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  validates :email, :username, :uniqueness => true
+  validates :username, :email, :first_name, :last_name, :presence => true
   # attr_accessible :title, :body
   has_many :tours
   has_many :bookings
