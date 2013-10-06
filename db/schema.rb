a0691579399803babbf7e598a6d269b6da282aed
quocle <quocle@no.reply.sorry.com>
2013-10-06 02:38:37 -0400

    Add phone to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131006042650) do
+ActiveRecord::Schema.define(:version => 20131006063750) do
 
   create_table "bookings", :force => true do |t|
     t.integer  "scheduled_tour_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20131006042650) do
     t.datetime "updated_at",                             :null => false
     t.string   "last_name"
     t.string   "first_name"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
