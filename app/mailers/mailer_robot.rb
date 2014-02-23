30f21b86725dc801e6124204dbe75a33f7aa2e7d
quocle <quocle@no.reply.sorry.com>
2014-02-23 11:25:13 -0500

    Format sender and email in Tour Starter confirmation

--- app/mailers/mailer_robot.rb
+++ app/mailers/mailer_robot.rb
@@ -1,5 +1,5 @@
 class MailerRobot < ActionMailer::Base
-  default from: "notification@tourstarter.com"
+  default :from => "\"Tour Starter Notification\" <tourstarter.notification@tourstarter.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -7,8 +7,9 @@ class MailerRobot < ActionMailer::Base
   #   en.mailer_robot.book_notification.subject
   #
   def book_notification booking
+    @booking = booking
     @first_name = booking.first_name == nil ? "" : booking.first_name
-    @greeting = "Hi, #{@first_name}. You successfully book this tour"
+    @greeting = "Hi, #{@first_name}. You successfully book the tour #{booking.scheduled_tour.tour.name} at #{booking.scheduled_tour.start_time}"
     mail :to => booking.email, :subject => "Tourstarter Confirmation"
   end
 end
